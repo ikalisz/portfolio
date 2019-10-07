5040256e699bc37fb5707560ecd664ec86292233
Ian Kaliszewski <48704792+ikalisz@users.noreply.github.com>
2019-10-06 20:57:02 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const app = express()
 const {SERVER_PORT} = process.env
 app.use(express.json())
 
-express.static(`${__dirname}/../src/assets`)
-express.static(`${__dirname}/../build`)
+app.use(express.static(`${__dirname}/../src/assets`))
+app.use(express.static(`${__dirname}/../build`))
 
 app.listen(SERVER_PORT, () => {
     console.log('running!')
